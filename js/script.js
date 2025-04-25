b82005d3c3de830e6ae0ae4932829e4041e34db0
imikkelsen <ingibjorgm1997@gmail.com>
2025-04-25 11:27:13 +0200

    Show and hide tooltip and audio

--- js/script.js
+++ js/script.js
@@ -7,6 +7,38 @@ document.addEventListener("DOMContentLoaded", function () {
     const anchor = document.querySelector(".anchor");
     const anchorTooltip = document.getElementById("anchor-tooltip");
     const audio = document.querySelector("audio");
-  });
-
   
+
+
+    anchor.addEventListener("mouseenter", () => {
+    const rect = anchor.getBoundingClientRect();
+    anchorTooltip.style.opacity = "1";
+    anchorTooltip.style.left = `${rect.left + 60}px`;
+    anchorTooltip.style.top = `${rect.top - 30}px`;
+
+    if (audio.paused) {
+        anchorTooltip.innerText = "Klik for at starte musikken";
+    } else {
+        anchorTooltip.innerText = "Klik for at stoppe musikken";
+    }
+    });
+
+
+    anchor.addEventListener("mouseleave", () => {
+        anchorTooltip.style.opacity = "0";
+    });
+
+    anchor.addEventListener("click", () => {
+        audio.muted = false;
+        if (audio.paused) {
+          audio.play();
+        } else {
+          audio.pause();
+        }
+      });
+
+
+
+
+
+});
