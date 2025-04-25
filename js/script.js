457963862bd31aaad959cd0f5f08bab6a0a5262b
imikkelsen <ingibjorgm1997@gmail.com>
2025-04-25 11:10:27 +0200

    JS grabbing HTML elements.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const anchor = document.querySelector(".anchor");
+    const anchorTooltip = document.getElementById("anchor-tooltip");
+    const audio = document.querySelector("audio");
+  });
+
+  
